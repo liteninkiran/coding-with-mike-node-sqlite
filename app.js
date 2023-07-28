4a056d8bd5453274a3c2504660eda78e89e27a93
Kiran Anand <liteninkiran@hotmail.com>
2023-07-29 00:47:02 +0100

    GET route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const res = require('express/lib/response');
 const app = express();
 const sqlite = require('sqlite3').verbose();
+const url = require('url');
 const db = new sqlite.Database('./quote.db', sqlite.OPEN_READWRITE, (err) => {
     if (err) {
         return console.log(err);
@@ -34,6 +35,40 @@ app.post('/quote', (req, res) => {
         });
     } catch(error) {
         console.log(error);
+        return res.json({
+            status: 400,
+            success: false,
+        });
+    }
+});
+
+app.get('/quote', (req, res) => {
+    sql = 'SELECT * FROM quote;';
+    try {
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                return res.json({
+                    status: 300,
+                    success: false,
+                    error: err,
+                });
+            }
+
+            if (rows.length < 1) {
+                return res.json({
+                    status: 300,
+                    success: false,
+                    error: 'No match',
+                });
+            }
+
+             return res.json({
+                status: 200,
+                success: true,
+                data: rows,
+             });
+        });
+    } catch (error) {
         return res.json({
             status: 400,
             success: false,
